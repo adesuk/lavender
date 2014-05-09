4e09c6c2f43ca2028032e6420752879c686b5476
ade sukendar <23513088@std.stei.itb.ac.id>
2014-05-09 10:11:42 +0700

    trouble with aplication server

--- Eclipse/jadwalKaWEB/src/main/java/ac/id/itb/ppl/lavender/managedbean/JadwalMBean.java
+++ Eclipse/jadwalKaWEB/src/main/java/ac/id/itb/ppl/lavender/managedbean/JadwalMBean.java
@@ -1,30 +1,39 @@
 package ac.id.itb.ppl.lavender.managedbean;
 
 import java.io.Serializable;
+import java.util.List;
 
 import javax.ejb.EJB;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+import javax.faces.event.AjaxBehaviorEvent;
 
 import ac.id.itb.ppl.lavender.bean.JadwalBean;
 import ac.id.itb.ppl.lavender.bean.PeriodeBean;
 import ac.id.itb.ppl.lavender.model.Periode;
+import ac.id.itb.ppl.lavender.model.view.JadwalView;
 import ac.id.itb.ppl.lavender.util.PeriodeFormat;
 import ac.id.itb.ppl.lavender.util.TipeEksekusi;
 
+@ManagedBean(name="jadwalMBean")
+@ViewScoped
 public class JadwalMBean implements Serializable {
 	
-	/**
-	 * 
-	 */
-	private static final long serialVersionUID = 1L;
-	
+	private static final long serialVersionUID = -4836165624248144491L;
 	@EJB
-	protected JadwalBean jadwalBean;
+	private JadwalBean jadwalBean;
 	@EJB
-	protected PeriodeBean periodeBean;
+	private PeriodeBean periodeBean;
 	
-	protected Periode periode;
-	protected TipeEksekusi tipeEksekusi;
+	private Periode periode;
+	private TipeEksekusi tipeEksekusi;
+	private List<Periode> periodes;
+	private List<JadwalView> jadwal;
 	
+	public JadwalMBean() {
+		tipeEksekusi = new TipeEksekusi();
+	}
+		
 	public String getPeriodeFormat() {
 		return "Periode ".concat(PeriodeFormat.format(periode));
 	}
@@ -34,13 +43,62 @@ public class JadwalMBean implements Serializable {
 				.concat(tipeEksekusi.getTipeEksekusi(
 							periode.getTipeJadwal().charAt(0)))
 				.concat("]");			
+	}	
+		
+	public List<JadwalView> getJadwals() {	
+		periode = periodeBean.lastPeriode();
+		return jadwalBean.findJadwalByPeriode(1);//p.getIdPeriode());
 	}
-
+	
+	public void reloadPeriodes() {
+		periodes = periodeBean.findAll();
+	}
+	
+	public String formatnya(Periode periode) {
+		return PeriodeFormat.format(periode);
+	}
+	 
+	public String getTipeJadwalRealName(char tipeJadwal) {
+		return tipeEksekusi.getTipeEksekusi(tipeJadwal);
+	}
+	    
+	public void cariListener(AjaxBehaviorEvent e) {
+		reloadJadwal();
+	}
+	    
+	public void reloadJadwal() {
+		System.out.println("Periode : "+ getPeriode().getIdPeriode());
+		jadwal = jadwalBean.findJadwalByPeriode(getPeriode().getIdPeriode());
+	}
+	
+	 // Getter dan setter
+    public List<Periode> getAllPeriodes() {
+        if (periodes == null || periodes.isEmpty()) {
+            reloadPeriodes();
+        }
+        return periodes;
+    }
+	
 	public Periode getPeriode() {
-		return periode;
+        if (periode == null && (periodes != null && !periodes.isEmpty())) {
+            periode = periodes.get(0);
+            System.out.println("if getSelectedPeriode");
+        }
+        System.out.println("not if getSelectedPeriode");
+        return periode;
+    }
+    
+    public void setPeriode(Periode periode) {
+    	System.out.println("setSelectedPeriode");
+        this.periode = periode;
+    }
+
+	public List<JadwalView> getJadwalsKoor() {
+		return jadwal;
 	}
 
-	public void setPeriode(Periode periode) {
-		this.periode = periode;
-	}
+	public void setJadwalsKoor(List<JadwalView> jadwal) {
+		this.jadwal = jadwal;
+	} 
+   
 }
